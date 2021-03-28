b6ea7f94910ecacd5d3f86539101c6d547f0059f
Jan Willem Buist <j.buist@fugro.com>
2021-03-28 12:22:14 +0200

    Added print method

--- pygedcom/elements.py
+++ pygedcom/elements.py
@@ -7,16 +7,18 @@ class Individual:
         self.lowerfam = None
 
         for line in gedcomlines:
+            # Save stuff into class
             if line.startswith('1 NAME'):
-                # Delete '1 NAME ' and '\n'
                 self.fullname = line[7:-2]
             elif line.startswith('1 SEX'):
-                # Delete '1 SEX ' and '\n', only keep M/F
                 self.sex = line[6]
             elif line.startswith('1 FAMS'):
                 self.lowerfam = line.split('@')[1]
             elif line.startswith('1 FAMC'):
                 self.upperfam = line.split('@')[1]
+
+    def __str__(self):
+        return '{}, {}'.format(self.fullname, self.sex)
 
 class Family:
     def __init__(self, gedcomlines):
