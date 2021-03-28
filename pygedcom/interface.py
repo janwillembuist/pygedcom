51d06cb2045afbe6427901ae8b6664b30e614327
janwillembuist <janwillembuist@gmail.com>
2021-03-28 20:59:43 +0200

    Added search functionality in GUI

--- pygedcom/interface.py
+++ pygedcom/interface.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from pygedcom.core import Parser
+
 
 class App(tk.Tk):
-    def __init__(self):
+    def __init__(self, gedcomfile):
         super().__init__()
 
         # Window settings
@@ -14,6 +16,9 @@ class App(tk.Tk):
         self.columnconfigure(1, weight=4)
         self.rowconfigure(0, weight=4)
         self.rowconfigure(1, weight=1)
+
+        # Load data
+        self.tree = Parser(gedcomfile).build_tree()
 
         # Create widgets
         self.__create_widgets()
@@ -35,11 +40,13 @@ class SearchFrame(ttk.Frame):
         self.columnconfigure(1, weight=3)
 
         # Create widgets
+        self.sv = tk.StringVar()
+        self.sv.trace('w', lambda name, index, mode, sv=self.sv: self.on_type_callback())
         self.__create_widgets()
 
     def __create_widgets(self):
         ttk.Label(self, text='Find person:').grid(column=0, row=0)
-        keyword = ttk.Entry(self, width=30)
+        keyword = ttk.Entry(self, width=30, textvariable=self.sv)
         keyword.focus()
         keyword.grid(column=1, row=0)
 
@@ -49,6 +56,9 @@ class SearchFrame(ttk.Frame):
         search_list.heading('#2', text='Backend ID')
         search_list.grid(column=0, row=1, columnspan=2)
 
+    def on_type_callback(self):
+        results = self.nametowidget('.').tree.find(self.sv.get())
+        print(results)
 
 class InfoFrame(ttk.Frame):
     def __init__(self, master):
--- pygedcom/main.py
+++ pygedcom/main.py
@@ -2,5 +2,6 @@ import tkinter as tk
 from pygedcom.interface import App
 
 if __name__ == '__main__':
-    app = App()
+    file = '../data/555SAMPLE.GED'
+    app = App(file)
     app.mainloop()
