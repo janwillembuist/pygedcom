a5b64baff5cb645620ac2d889d9e5417af83b428
janwillembuist <janwillembuist@gmail.com>
2021-03-30 21:12:06 +0200

    Added callback from select person to InfoFrame

--- pygedcom/interface.py
+++ pygedcom/interface.py
@@ -20,16 +20,16 @@ class App(tk.Tk):
         # Load data
         self.tree = Parser(gedcomfile).build_tree()
 
-        # Create widgets
-        self.__create_widgets()
+        # Placeholder for selected person
+        self.selected_individual = None
 
-    def __create_widgets(self):
-        search_frame = SearchFrame(self)
-        search_frame.grid(column=0, row=0, rowspan=2)
-        main_frame = MainFrame(self)
-        main_frame.grid(column=1, row=0)
-        info_frame = InfoFrame(self)
-        info_frame.grid(column=1, row=1)
+        # Create subframes
+        self.search_frame = SearchFrame(self)
+        self.search_frame.grid(column=0, row=0, rowspan=2)
+        self.main_frame = MainFrame(self)
+        self.main_frame.grid(column=1, row=0)
+        self.info_frame = InfoFrame(self)
+        self.info_frame.grid(column=1, row=1)
 
 class SearchFrame(ttk.Frame):
     def __init__(self, master):
@@ -57,6 +57,9 @@ class SearchFrame(ttk.Frame):
         self.treeview_widget.heading('#2', text='Backend ID')
         self.treeview_widget.grid(column=0, row=1, columnspan=2)
 
+        self.select_button = ttk.Button(self, text='Select person', command=self.select_callback)
+        self.select_button.grid(column=0, row=2)
+
     def on_type_callback(self):
         self.search_results = self.nametowidget('.').tree.find(self.sv.get())
 
@@ -68,15 +71,33 @@ class SearchFrame(ttk.Frame):
         for res in self.search_results:
             self.treeview_widget.insert('', 0, values=(res, 'backend ID'))
 
+    def select_callback(self):
+        selected = self.treeview_widget.focus()
+        selected = self.treeview_widget.item(selected)['values'][0]
+
+        # Find corresponding ID and save in main app
+        person_id = self.nametowidget('.').tree.individuals_lookup[selected]
+        self.nametowidget('.').selected_individual = person_id
+
+        # Update info and main frame
+        self.nametowidget('.!infoframe').update_callback(person_id)
+
+
 class InfoFrame(ttk.Frame):
     def __init__(self, master):
         super().__init__(master)
 
-        # Create widgets
-        self.__create_widgets()
+        # Markup info
+        self.infostring = tk.StringVar()
+        self.infostring.set('None')
 
-    def __create_widgets(self):
-        pass
+        # Create widgets
+        self.label = ttk.Label(self, textvariable=self.infostring)
+        self.label.pack()
+
+    def update_callback(self, person_id):
+        individual = self.nametowidget('.').tree.individuals[person_id]
+        self.infostring.set(individual)
 
 class MainFrame(ttk.Frame):
     def __init__(self, master):
